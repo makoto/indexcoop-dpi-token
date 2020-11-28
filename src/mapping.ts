9b5c486781bf38f5656a394fb8e8fc0eedf9fcd3
Makoto Inoue <2630+makoto@users.noreply.github.com>
2020-11-28 21:12:45 +0000

    Initical commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,134 +1,34 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Contract,
-  Approval,
-  ComponentAdded,
-  ComponentRemoved,
-  DefaultPositionUnitEdited,
-  ExternalPositionDataEdited,
-  ExternalPositionUnitEdited,
-  Invoked,
-  ManagerEdited,
-  ModuleAdded,
-  ModuleInitialized,
-  ModuleRemoved,
-  PendingModuleRemoved,
-  PositionModuleAdded,
-  PositionModuleRemoved,
-  PositionMultiplierEdited,
-  Transfer
+  Contract, Approval,
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { IndexEntity } from "../generated/schema"
+import { log } from '@graphprotocol/graph-ts'
 
 export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let contract = Contract.bind(event.address)
+  log.warning(
+    '*** 1 Block number: {}, block hash: {}, transaction hash: {}',
+    [
+      event.block.number.toString(),
+      event.block.hash.toHexString(),
+      event.transaction.hash.toHexString()
+    ]
+  );
+  let positions = contract.getPositions()
+  for (let i = 0, l = positions.length; i < l; ++i) {
+    let positon = positions[i]
+    let entity = new IndexEntity(event.transaction.hash.toHex() + "-" + i.toString())
+    entity.component = positon.component
+    entity.unit = positon.unit
+    entity.timestamp = event.block.timestamp
+    entity.save()
+    log.warning(
+      '*** 2 i: {}, component: {}, unit: {}',
+      [
+        i.toString(),
+        entity.component.toHexString(),
+        entity.unit.toHexString()
+      ]
+    );
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.components(...)
-  // - contract.controller(...)
-  // - contract.decimals(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.getComponents(...)
-  // - contract.getDefaultPositionRealUnit(...)
-  // - contract.getExternalPositionData(...)
-  // - contract.getExternalPositionModules(...)
-  // - contract.getExternalPositionRealUnit(...)
-  // - contract.getModules(...)
-  // - contract.getPositions(...)
-  // - contract.getTotalComponentRealUnits(...)
-  // - contract.increaseAllowance(...)
-  // - contract.invoke(...)
-  // - contract.isComponent(...)
-  // - contract.isExternalPositionModule(...)
-  // - contract.isInitializedModule(...)
-  // - contract.isLocked(...)
-  // - contract.isPendingModule(...)
-  // - contract.locker(...)
-  // - contract.manager(...)
-  // - contract.moduleStates(...)
-  // - contract.modules(...)
-  // - contract.name(...)
-  // - contract.positionMultiplier(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
 }
-
-export function handleComponentAdded(event: ComponentAdded): void {}
-
-export function handleComponentRemoved(event: ComponentRemoved): void {}
-
-export function handleDefaultPositionUnitEdited(
-  event: DefaultPositionUnitEdited
-): void {}
-
-export function handleExternalPositionDataEdited(
-  event: ExternalPositionDataEdited
-): void {}
-
-export function handleExternalPositionUnitEdited(
-  event: ExternalPositionUnitEdited
-): void {}
-
-export function handleInvoked(event: Invoked): void {}
-
-export function handleManagerEdited(event: ManagerEdited): void {}
-
-export function handleModuleAdded(event: ModuleAdded): void {}
-
-export function handleModuleInitialized(event: ModuleInitialized): void {}
-
-export function handleModuleRemoved(event: ModuleRemoved): void {}
-
-export function handlePendingModuleRemoved(event: PendingModuleRemoved): void {}
-
-export function handlePositionModuleAdded(event: PositionModuleAdded): void {}
-
-export function handlePositionModuleRemoved(
-  event: PositionModuleRemoved
-): void {}
-
-export function handlePositionMultiplierEdited(
-  event: PositionMultiplierEdited
-): void {}
-
-export function handleTransfer(event: Transfer): void {}
